1ab557c63da156a4dda3742a7002e2424d28ff13
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2023-06-09 09:25:51 +0530

    intial push

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <div class="grid grid-cols-4 gap-4">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+            <div>04</div>
+            <div>05</div>
+            <div>06</div>
+            <div>07</div>
+            <div>09</div>
+        </div>
+    )
+}
+
+export default Header
